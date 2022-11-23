a79a337ad3317ce1b7a4fd87cb53a71ebf1ff10b
dyakonovr <dyakonov.rr@bk.ru>
2022-11-23 23:04:13 +0400

    сделал основу (вид айфона) для приложения

---
+++ src/components/LoginScreen/LoginScreen.jsx
@@ -0,0 +1,9 @@
+import classes from './LoginScreen.module.css';
+
+const LoginScreen = () => {
+  return (
+    <div style={{backgroundColor: "red"}}></div>
+  );
+};
+
+export default LoginScreen;
---
+++ src/components/Phone/Phone.jsx
@@ -0,0 +1,20 @@
+import classes from './Phone.module.css';
+
+const Phone = ({ children }) => {
+  return (
+    <div className={classes.phone}>
+      <div className={classes.header}>
+        <p className={classes.provider}>MegaFon</p>
+        <div className={classes.unibrow}></div>
+        <div className={classes.battery}>
+          <img src="./src/assets/battery.svg" alt="Иконка зарядки" />
+        </div>
+      </div>
+      <div className={classes.application}>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Phone;
