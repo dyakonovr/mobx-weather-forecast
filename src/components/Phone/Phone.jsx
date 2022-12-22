8903c68296fd254a101f603bda4f9fd33668da22
dyakonovr <dyakonov.rr@bk.ru>
2022-12-22 23:23:37 +0400

    добавил время в шапку айфона

--- src/components/Phone/Phone.jsx
+++ src/components/Phone/Phone.jsx
@@ -1,11 +1,19 @@
 import classes from './Phone.module.css';
 
 const Phone = ({ children }) => {
+  // Функции
+  function getTime() {
+    const time = new Date();
+    return `${time.getHours()}:${time.getMinutes()}`
+  }
+  // Функции END
+
   return (
     <div className={classes.phone}>
       <div className={classes.header}>
         <p className={classes.provider}>MegaFon</p>
         <div className={classes.unibrow}></div>
+        <span className={classes.time}>{getTime()}</span>
         <div className={classes.battery}>
           <img src="./src/assets/battery.svg" alt="Иконка зарядки" />
         </div>
