4ecbfa4034f009cc89564de4c5746a44b7e24895
dyakonovr <dyakonov.rr@bk.ru>
2023-01-02 22:11:46 +0400

    добавил закрытие <Popup /> по Enter'у

--- src/components/UI/Popup/Popup.jsx
+++ src/components/UI/Popup/Popup.jsx
@@ -1,8 +1,23 @@
 import classes from './Popup.module.css'
+import { useEffect } from 'react';
 import popupStore from '../../../store/popupStore';
 import AnimationPage from '../../AnimatedPage/AnimatedPage';
 
 function Popup() {
+  // Обрабатываю нажатие на Enter
+  useEffect(() => {
+    function handleKeybordPress(e) {
+      if (e.key === 'Enter') popupStore.togglePopup();
+    }
+    document.addEventListener('keyup', handleKeybordPress);
+
+    // При окончании авторизации - "открутить" слушатель
+    return function () {
+      document.removeEventListener('keyup', handleKeybordPress);
+    }
+  });
+  // Обрабатываю нажатие на Enter END
+
   return (
     <AnimationPage>
       <div className={classes.popup} onClick={() => popupStore.togglePopup()}>
